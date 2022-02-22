78f317d6bda3f0b2691ff3bc7b02291fc32a91ae
TYama <alohaty81@gmail.com>
2022-02-22 10:25:18 -0700

    fixed the first best function false cases

--- stockPrices.js
+++ stockPrices.js
@@ -23,13 +23,13 @@ best([100, 0])
 
 //get the length of the array
 //check the highest value of the day
-//splice(indexOfHighest, (length - 1) - indexOfHighest)
+//slice(indexOfHighest, (length - 1) - indexOfHighest) -> keep making new array
 //if the length of the new array === 1 -> return 0 benefit
 //else -> check the lowest value of the day in the modified array
 //highest value - lowest value
 //return the maximam profit, highest value, and lowest value
 
-let best = stockPrices => {
+let best1 = stockPrices => {
     let highestPrice = getHighestPrice(stockPrices);
     let highestPriceIndex = stockPrices.indexOf(highestPrice);
 
@@ -37,12 +37,14 @@ let best = stockPrices => {
         return 0;
     }else{
         //modify the array to make the last element the highestPrice
-        stockPrices.splice(highestPriceIndex, (stockPrices.length - 1));
+        let newArr = stockPrices.slice(highestPriceIndex, (stockPrices.length - 1));
     }
     //console.log(stockPrices);
-    let lowestPrice = getLowestPrice(stockPrices);
+    let lowestPrice = getLowestPrice(newArr);
     return highestPrice - lowestPrice;
 }
+
+
 
 let getHighestPrice = arr => {
     let highestPrice = arr[0];
@@ -63,5 +65,25 @@ let getLowestPrice = arr => {
     }
     return lowestPrice;
 }
-let test = [99, 100, 1, 24, 82, 90, 12, 39];
-console.log(best(test));
+let test = [101, 100, 99] //, 1, 24, 82, 90, 12, 39];
+
+let best2 = stockPrices => {
+    let maxBenefit = 0;
+    let lowest = stockPrices[0];
+
+    for(let i = 1; i < stockPrices.length; i++){
+        if(stockPrices[i] < lowest){
+            lowest = stockPrices[i];
+        }
+        
+        let possibleBenefit = stockPrices[i] - lowest;
+        if(possibleBenefit > maxBenefit){
+            maxBenefit = possibleBenefit;
+        }
+    }
+    return maxBenefit;  
+    
+}
+
+console.log(best2(test));
+
